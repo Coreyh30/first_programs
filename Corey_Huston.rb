287ab6702d92a229a5a59ce7b993960ec42883ea
Coreyh30 <coreyhuston30@gmail.com>
2019-11-02 20:12:59 +0000

    my second commit

---
+++ Corey_Huston.rb
@@ -0,0 +1,11 @@
+puts "First name"
+a = []
+user_input = gets.chomp
+a << user_input
+puts "Middle"
+user_input = gets.chomp
+a << user_input
+puts "Last"
+user_input = gets.chomp
+a << user_input
+puts a.inspect
